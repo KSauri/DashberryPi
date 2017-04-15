1d23427291a40477a57e2cc7c0ac61b395abfa8f
nselvidge <nselvidge@gmail.com>
2017-04-15 18:11:04 -0400

    Added Google Event Component

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ const electron = require('electron');
 const app = electron.app;
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow;
-
 const path = require('path');
 const url = require('url');
+require('dotenv').config();
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import React, { Component } from 'react';
 import { Col } from 'react-bootstrap';
+import GoogleCalendar from './lib/GoogleCalendar.js';
+import EventsSummary from './components/EventsSummary.jsx';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      eventData: {
+        isLoading: true
+      }
+    }
+    this.getEventData();
+  }
+  async getEventData() {
+    const gc = new GoogleCalendar();
+    let data = await gc.getData();
+    console.log(data);
+    this.setState({
+      eventData: {
+        isLoading: false,
+        data
+      }
+    });
+  }
   render() {
     return (
       <div className="App">
@@ -10,7 +32,7 @@ class App extends Component {
           <h3>Subheader </h3>
         </Col>
         <Col sm={4}>
-          <h3>Subheader </h3>
+          <EventsSummary isLoading={this.state.eventData.isLoading} data={this.state.eventData.data || []} />
         </Col>
         <Col sm={4}>
           <h3>Subheader </h3>
