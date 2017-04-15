c262522c61b2c1ce7f2ac09f161baf05d6f98f03
nselvidge <nselvidge@gmail.com>
2017-04-15 15:52:30 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Col } from 'react-bootstrap';
 import logo from './logo.svg';
 import './css/App.css';
 
@@ -6,13 +7,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Holy Butts</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Holy Butt Dashboard</h1>
+        <Col sm={4}>
+          <h3>Subheader </h3>
+        </Col>
+        <Col sm={4}>
+          <h3>Subheader </h3>
+        </Col>
+        <Col sm={4}>
+          <h3>Subheader </h3>
+        </Col>
       </div>
     );
   }
