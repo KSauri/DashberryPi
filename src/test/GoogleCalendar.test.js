cf6173539a8a457eeb3e6f786817a98f35a2db5f
nselvidge <nselvidge@gmail.com>
2017-04-15 18:11:04 -0400

    WIP

---
+++ src/test/GoogleCalendar.test.js
@@ -0,0 +1,11 @@
+import GoogleCalendar from '../lib/GoogleCalendar';
+
+
+// test('Should load calendar data', async () => {
+//   const gc = new GoogleCalendar();
+//   await gc.getData();
+// })
+
+test('Should load calendar data', async () => {
+  return true;
+})
