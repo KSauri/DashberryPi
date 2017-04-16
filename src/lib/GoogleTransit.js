f410f98c61d392a62948eff4b706c5823838efa2
Kyle Sauri <kylesauri@gmail.com>
2017-04-16 19:11:15 -0400

    Add normal depart time, depart now time for travel

--- src/lib/GoogleTransit.js
+++ src/lib/GoogleTransit.js
@@ -1,37 +1,69 @@
 import rp from 'request-promise';
+import moment from 'moment';
 
+// let moment = require("moment");
+
+// export default
 
 export default class GoogleTransit {
-  constructor(origin, destination) {
+  constructor(origin, destination, departTime) {
     this.origin = origin;
     this.destination = destination;
+    this.departTime = departTime;
   }
 
-  haveFun () {
-    console.log("so fun");
+  _getDepartTimeUnixEpoch () {
+    let now = moment().valueOf();
+    let departTimeToday = moment().startOf('day').add(this.departTime, 'h').valueOf()/1000;
+    let departTimeTomorrow = moment().startOf('day').add(this.departTime + 24, 'h').valueOf()/1000;
+    if (now > departTimeToday) {
+      return departTimeTomorrow;
+    } else {
+      return departTimeToday;
+      }
   }
 
-  async getData() {
+  async _getDataDepartNormal() {
     try {
       let data = await rp({
         method: "GET",
-        url: `https://maps.googleapis.com/maps/api/directions/json?origin=${this.origin}&destination=${this.destination}&mode=transit&key=AIzaSyCyVBmd0JigVvVazx5O6e3OHwopFn0tklY`,
+        url: `https://maps.googleapis.com/maps/api/directions/json?` +
+          `origin=${this.origin}&destination=${this.destination}&` +
+          `departure_time=${this._getDepartTimeUnixEpoch()}&mode=transit` +
+          `&key=AIzaSyCyVBmd0JigVvVazx5O6e3OHwopFn0tklY`,
         json: true
       });
-      console.log(data.routes[0].legs);
+      return data;
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  async _getDataDepartNow() {
+    try {
+      let data = await rp({
+        method: "GET",
+        url: `https://maps.googleapis.com/maps/api/directions/json?` +
+          `origin=${this.origin}&destination=${this.destination}&` +
+          `mode=transit&key=AIzaSyCyVBmd0JigVvVazx5O6e3OHwopFn0tklY`,
+        json: true
+      });
       return data;
     } catch (err) {
       console.error(err);
     }
   }
 
+
   async getParsedDistance() {
-    let rawData = await this.getData();
+    let rawData = await this._getDataDepartNow();
     return rawData.routes[0].legs[0].distance.text;
   }
-
-  async getParsedDuration() {
-    let rawData = await this.getData();
+  async getParsedDurationNormal() {
+    let rawData = await this._getDataDepartNormal();
+    return rawData.routes[0].legs[0].duration.text;
+  }
+  async getParsedDurationNow() {
+    let rawData = await this._getDataDepartNow();
     return rawData.routes[0].legs[0].duration.text;
   }
 }
--- src/test/GoogleTransit.test.js
+++ src/test/GoogleTransit.test.js
@@ -3,10 +3,18 @@ import GoogleTransit from '../lib/GoogleTransit';
 
 test('Should load transit data', async () => {
   let gt = new GoogleTransit("3 Old Army Road, Bernardsville","160 Varick Street, New York");
-  return gt.getData();
+  return gt._getDataDepartNow();
 })
 test("Should return the right distance from B'ville to Varick", async () => {
   let gt = new GoogleTransit("3 Old Army Road, Bernardsville","160 Varick Street, New York");
   let distance = await gt.getParsedDistance();
   expect(distance).toBe("39.9 mi");
+})
+test("Log the time to leave now vs normal time", async () => {
+  let gt = new GoogleTransit("3 Old Army Road, Bernardsville","160 Varick Street, New York", 9);
+  let departNow = await gt.getParsedDurationNow();
+  let departNormal = await gt.getParsedDurationNormal();
+  console.log(departNow);
+  console.log(departNormal);
+  return true;
 })
